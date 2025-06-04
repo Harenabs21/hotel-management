646c95b50bae60788430451fe92da16d0e230041
Harenabs21 <hei.harena.6@gmail.com>
2025-06-04 22:57:41 +0300

    import all models

--- hotel/models/__init__.py
+++ hotel/models/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import hotel_customer
+from . import hotel_room
+from . import hotel_room_booking
+from . import hotel_room_equipment
+from . import hotel_room_type
